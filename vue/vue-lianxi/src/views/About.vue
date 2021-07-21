455793a94514ae34b712d36068450f8cecdf56bb
龙\红尘一诺 <1520090739@qq.com>
2021-07-21 09:44:52 +0800

    数字滚动效果

--- vue/vue-lianxi/src/views/About.vue
+++ vue/vue-lianxi/src/views/About.vue
@@ -1,5 +1,50 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <ul class="flex">
+    <ScrollNum
+      v-for="(number, idx) of numArr"
+      :key="idx"
+      :i="number"
+      :delay="idx + 1"
+      as="li"
+      class="num"
+    />
+  </ul>
 </template>
+
+<script>
+import ScrollNum from "../components/ScrollNum.vue";
+
+export default {
+  name: "App",
+  components: { ScrollNum },
+  data: () => ({ num: 15798002275 }),
+  computed: {
+    numArr() {
+      let arr = [];
+      const str = String(this.num);
+      for (let index = 0; index < str.length; index++) {
+        arr[index] = parseInt(str[index]);
+      }
+      return arr;
+      // return [parseInt(str[0]), parseInt(str[1]), parseInt(str[2])];
+    },
+  },
+};
+</script>
+
+<style>
+.flex {
+  display: flex;
+}
+ul {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+.num {
+  --width: 26px;
+  margin-right: 6px;
+  border: 1px solid black;
+  border-radius: 8px;
+}
+</style>
